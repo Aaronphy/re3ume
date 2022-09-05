3cbdf8a591f2e48f39c4f1110f58b87670b63467
Aaronphy <aaronphy@AaronphydeMacBook-Pro.local>
2022-09-05 23:04:11 +0800

    chore: init workspace

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.less';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1 className="title">Hello React + TypeScript!</h1>
+    </div>
+  );
+};
+
+export default App;
